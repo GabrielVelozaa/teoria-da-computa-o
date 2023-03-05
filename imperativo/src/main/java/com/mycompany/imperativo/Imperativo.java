faf0fac835ceca1fd2eaf608e987ef6df8887539
unknown <gabrielvelozoanjosss@gmail.com>
2023-03-04 23:07:39 -0300

    first commit

---
+++ imperativo/src/main/java/com/mycompany/imperativo/Imperativo.java
@@ -0,0 +1,22 @@
+
+
+package com.mycompany.imperativo;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] numerosPares = new int[numeros.length];
+        int contador = 0;
+        for (int numero : numeros) {
+            if (numero % 2 == 0) {
+                numerosPares[contador] = numero;
+                contador++;
+            }
+        }
+        int[] resultado = new int[contador];
+        for (int i = 0; i < contador; i++) {
+            resultado[i] = numerosPares[i];
+        }
+        System.out.println(Arrays.toString(resultado));
+    }
+}
